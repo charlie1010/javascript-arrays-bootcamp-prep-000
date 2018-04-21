912eee5439350f2ba69d81916ae4f56d504011ee
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-21 14:30:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,17 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
-function addElementToBeginningOfArray() {
+function addElementToBeginningOfArray(array,element) {
+  return array.unshift()
+}
+
+function destructivelyAddElementToBeginningOfArray(array,element) {
   return myArray.unshift()
 }
 
-function destructivelyAddElementToBeginningOfArray() {
-  return myArray.unshift()
-}
-
-function addElementToEndOfArray() {
+function addElementToEndOfArray(array,element) {
   
 }
 
-function destructivelyAddElementToEndOfArray() {
+function destructivelyAddElementToEndOfArray(array,element) {
   
 }
