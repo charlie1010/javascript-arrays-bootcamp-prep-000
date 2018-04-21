5bc9c459e880fbc178afbae88470922918dfdd27
CHARLIE MCCORMICK <chamccor9692@gmail.com>
2018-04-21 16:05:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -22,10 +22,11 @@ function accessElementInArray(array,index) {
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
   array.shift()
+  array
 }
 
 function removeElementFromBeginningOfArray (array) {
-  array.slice(0)
+  array.slice(1)
 }
 
 function destructivelyRemoveElementFromEndOfArray (array) {
